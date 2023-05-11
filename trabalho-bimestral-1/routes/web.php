a3a1457207ed35e9637fb69b5e7a3317a4e4f932
MatHennel <matheushennel123@gmail.com>
2023-05-11 20:58:32 -0300

    adicionado a rota para classe EixoController

--- trabalho-bimestral-1/routes/web.php
+++ trabalho-bimestral-1/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EixoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('eixos','EixoController');
+
+
+
+
